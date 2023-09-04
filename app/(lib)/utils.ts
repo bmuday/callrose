813cfac9063046a63055ed116805218222180fed
bmuday <bmuday@live.fr>
2023-09-04 19:47:47 -0400

    deploy1

--- app/(lib)/utils.ts
+++ app/(lib)/utils.ts
@@ -5,49 +5,6 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const getCurrentUser = async (access_token) => {
-  const endpoint = "/users/me";
-  let options;
-  if (access_token)
-    options = {
-      headers: {
-        Authorization: `Bearer ${access_token}`,
-      },
-    };
-  try {
-    const { data } = await fetchDirectus(endpoint, options);
-    return data;
-  } catch (error) {
-    console.log("error", error);
-    return error;
-  }
-};
-
-export const setCookie = (name, value, days) => {
-  var expires = "";
-  if (days) {
-    var date = new Date();
-    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-    expires = "; expires=" + date.toUTCString();
-  }
-  document.cookie = name + "=" + (value || "") + expires + "; path=/";
-};
-
-export const getCookie = (name) => {
-  var nameEQ = name + "=";
-  var ca = document.cookie.split(";");
-  for (var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == " ") c = c.substring(1, c.length);
-    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-  }
-  return null;
-};
-
-export const deleteCookie = (name) => {
-  document.cookie = name + "=; Max-Age=0";
-};
-
 export function roomUrlFromPageUrl() {
   const match = window.location.search.match(/roomUrl=([^&]+)/i);
   return match && match[1] ? decodeURIComponent(match[1]) : null;
